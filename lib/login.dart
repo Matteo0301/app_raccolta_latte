c1c9c846881bad6efc71c2b9586db5282608c745
Matteo0301 <matteomariotti0301@gmail.com>
2024-01-08 11:39:25 +0100

    Remember password when logging in

--- lib/login.dart
+++ lib/login.dart
@@ -27,100 +27,97 @@ class LoginState extends State<Login> {
         key: _formKey,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: AutofillHints.username),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Inserisci il nome utente';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: AutofillHints.password),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Inserisci la password';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
-                child: Center(
-                  child: ElevatedButton(
-                    /* style: ButtonStyle(
-                      textStyle: MaterialStateProperty.all(
-                          Theme.of(context).textTheme.headlineSmall),
-                      padding: MaterialStateProperty.all(
-                          const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 14)),
-                    ), */
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        // request to the server
-                        String username = emailController.text;
-                        String password = passwordController.text;
-
-                        loginRequest(username, password).then(
-                          (user) async {
-                            log('user: $user');
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Home(
-                                        title: widget.title,
-                                        username: user.username,
-                                        admin: user.admin,
-                                      )),
-                            );
-                          },
-                        ).onError(
-                          (error, stackTrace) async {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text(error.toString())),
-                            );
-                          },
-                        );
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content: Text(
-                                  'Non hai inserito i dati correttamente')),
-                        );
+          child: AutofillGroup(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextFormField(
+                    autofillHints: const [AutofillHints.username],
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: AutofillHints.username),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Inserisci il nome utente';
                       }
+                      return null;
                     },
-                    child: const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text('Login', style: TextStyle(fontSize: 20)),
-                    ),
-                    /* const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text('Login', style: TextStyle(fontSize: 20)),
-                    ), */
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextFormField(
+                    autofillHints: const [AutofillHints.password],
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: AutofillHints.password),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Inserisci la password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          // request to the server
+                          String username = emailController.text;
+                          String password = passwordController.text;
+
+                          loginRequest(username, password).then(
+                            (user) async {
+                              log('user: $user');
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Home(
+                                          title: widget.title,
+                                          username: user.username,
+                                          admin: user.admin,
+                                        )),
+                              );
+                            },
+                          ).onError(
+                            (error, stackTrace) async {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text(error.toString())),
+                              );
+                            },
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                                    'Non hai inserito i dati correttamente')),
+                          );
+                        }
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text('Login', style: TextStyle(fontSize: 20)),
+                      ),
+                      /* const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text('Login', style: TextStyle(fontSize: 20)),
+                    ), */
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
